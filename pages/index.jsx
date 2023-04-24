8d0848f4c86289a26b1fcdf74a5944f11e570fcb
jollypyun <lucas.stave@gmail.com>
2023-04-24 17:30:33 +0900

    renamed:    pages/_app.tsx -> pages/_app.jsx 	renamed:    pages/_document.tsx -> pages/_document.jsx 	renamed:    pages/blog/[postId].tsx -> pages/blog/[postId].jsx 	renamed:    pages/blog/important.tsx -> pages/blog/important.jsx 	renamed:    pages/blog/index.tsx -> pages/blog/index.jsx 	renamed:    pages/index.tsx -> pages/index.jsx 	renamed:    pages/news.tsx -> pages/news.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList'
 
-let dummyMeetups: object[] = [
+const dummyMeetups = [
     {
         id: 'm1',
         title: 'First',
@@ -18,15 +18,21 @@ let dummyMeetups: object[] = [
     }
 ]
 
+// 두 번의 사이클을 돌면 NextJS는 두번째 사이클에 대한 것을 기다려주지 않는다.
+// 문제는 그곳에 렌더링된 HTML 컨텐츠가 담겨져 있다.
+// 첫번째 사이클의 HTML 컨텐츠에는 내용에 해당하는 HTML이 존재하지 않는다.
+// 사전 렌더링이 요청한 데이터를 기다리지 않기 때문이다. 그러면 이것을 해결하려면? 
 function HomePage() {
     const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-    // useEffect(() => {
-    //     setLoadedMeetups(dummyMeetups);
-    // }, []);
-    return <MeetupList meetups={dummyMeetups} />
+    useEffect(() => {
+        setLoadedMeetups(dummyMeetups);
+    }, []);
+    return <MeetupList meetups={loadedMeetups} />
 }
 
 export default HomePage;
 
 // root 경로
+
+// 
